7d5f4c381251200010e56325246c2ea86b6e95e3
Anurag <vizculus@gmail.com>
2021-04-10 17:15:57 +0545

    Added gallery

--- Client/src/components/GalleryX.js
+++ Client/src/components/GalleryX.js
@@ -1,15 +1,38 @@
-
-
 import './GalleryX.css' 
-
-
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Gallery from 'react-photo-gallery'
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+import Overlay from "react-overlay-component";
 
 const GalleryX = () => {
+    const [isOpen, setOverlay] = useState(false);
+    const imgList= [
+      'https://homepages.cae.wisc.edu/~ece533/images/airplane.png',
+      'https://homepages.cae.wisc.edu/~ece533/images/arctichare.png',
+      'https://homepages.cae.wisc.edu/~ece533/images/baboon.png',
+      'https://homepages.cae.wisc.edu/~ece533/images/boat.png',
+      'https://homepages.cae.wisc.edu/~ece533/images/fruits.png',
+    ];
+    function opencarousel(){
+      setOverlay(true);
+    }
+    const configs = {
+      animate: true,
+      top: `0em`,
+    };
     return (
         <div>
-            <h1> Gallery</h1>
-        </div>
+        <h1> Gallery</h1>
+	   <Carousel showArrows={true} width="37%" infiniteLoop>
+            {imgList.map((element) =>
+	      <div>
+	        <img src={element}/>
+	        <p className="legend">Cement</p>
+	      </div>
+	    )}
+           </Carousel> 
+      </div>
     )
 }
 
