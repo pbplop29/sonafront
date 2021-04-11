66cc8247220bd18c4dbac4dc04885282553b41ab
Your Nameemail <pbplop@gmail.com>
2021-04-11 12:24:50 +0530

    added react-images and react-phot-gallery to about page and photos.js is a part of it

--- Client/src/components/About.js
+++ Client/src/components/About.js
@@ -1,13 +1,17 @@
-import React from 'react'
+
 import Header from './Header'
 import MenuX from './MenuX'
-const About = () => {
-    return (
-        <div>
-            <MenuX />
-            <Header />
-        </div>
-    )
-}
+import React from "react";
 
+import Gallery from "react-photo-gallery";
+import { photos } from "./photos";
+
+const About = () => 
+<div>
+    <MenuX />
+    <Header />
+<Gallery photos={photos} />;
+</div>
 export default About
+
+
