ad82a96a255e4377decb2d7acb81831977c0e32e
Your Nameemail <pbplop@gmail.com>
2021-04-16 21:46:47 +0530

    changed responsiveness of contact and admin page, also added gradient effect to background

--- Client/src/components/About.js
+++ Client/src/components/About.js
@@ -177,13 +177,7 @@ const About = () =>
   <h1>Skewed Clockwise.</h1>
   <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</p>
 </div>
-  </div>
-
-
-
-
-
-
+</div>
 
 
 
