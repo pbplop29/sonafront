3122d06aed2c5fa9b92f758490fd7eaa36ec7187
Your Nameemail <pbplop@gmail.com>
2021-04-16 17:22:08 +0530

    check responsiveness of about page

--- Client/src/components/About.js
+++ Client/src/components/About.js
@@ -70,7 +70,7 @@ const About = () =>
 
 
 
-
+    <div className="pc__screen">
 
     <div class="skw-pages">
   <div class="skw-page skw-page-1 active">
@@ -151,22 +151,33 @@ const About = () =>
         </div>
       </div>
     </div>
+    </div>
+
+    
+    </div>
   </div>
+
+
+
+  <div className="mobile__screen">
+  <link href='https://fonts.googleapis.com/css?family=Wire+One|Raleway:300' rel='stylesheet' type='text/css'></link>
+
+<div class="skew-c"></div>
+<div class="colour-block">
+  <h1>Responsive Skewed Page Dividers.</h1>
+  <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</p>
 </div>
-
-
-
-
-
-
-    
-
-    
-
-
-
-
-
+<div class="skew-cc"></div>
+<div class="white-block">
+  <h1>Skewed Counter-Clockwise.</h1>
+  <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</p>
+</div>
+<div class="skew-c"></div>
+<div class="colour-block">
+  <h1>Skewed Clockwise.</h1>
+  <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</p>
+</div>
+  </div>
 
 
 
