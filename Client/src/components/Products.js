572c94651bf79f70782d15dbb840ee7cd4d50c40
Your Nameemail <pbplop@gmail.com>
2021-04-19 09:12:05 +0530

    added more stuff to products page

--- Client/src/components/Products.js
+++ Client/src/components/Products.js
@@ -4,6 +4,7 @@ import MenuX from './MenuX'
 import React from 'react'
 import sonasac from '../assets/sonasac.png'
 import sonasac2 from '../assets/sonasac2.png'
+import Clinker from '../assets/Clinker.png'
 const Products = () => {
     return (
         <div>
@@ -25,9 +26,8 @@ const Products = () => {
                     <div className="content">
                         <h2 className='product__title'>Sona</h2>
                         <span className='product__desc'>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat, eos? 
-                            Rerum odit reiciendis aperiam deserunt molestias hic
-                            ex eveniet. Magnam, voluptatem dolor corporis iste r
+                        Ordinary Porrtland Cement (OPC) <br></br>
+
                             
                         </span>
 
@@ -45,9 +45,7 @@ const Products = () => {
                     <div className="content">
                         <h2 className='product__title'>Sona Shree</h2>
                         <span className='product__desc'>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat, eos? 
-                            Rerum odit reiciendis aperiam deserunt molestias hic
-                            ex eveniet. Magnam, voluptatem dolor corporis iste r
+                        Pozzolana Portland Cement (PPC)
                             
                         </span>
 
@@ -58,6 +56,23 @@ const Products = () => {
             </div>
             
 
+
+            <div className="card">
+                <div className="circle3">
+                    <div className="content">
+                        <h2 className='product__title3'>Clinker</h2>
+                        <span className='product__desc3'>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat, eos? 
+                            Rerum odit reiciendis aperiam deserunt molestias hic
+                            ex eveniet. Magnam, voluptatem dolor corporis iste r
+                            
+                        </span>
+
+                    </div>
+                    
+                </div>
+                <img src={Clinker} alt=""/>
+            </div>
 
 
 
