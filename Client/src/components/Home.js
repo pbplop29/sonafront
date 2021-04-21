7510002799d9edd470d583cc23122cc3e8f94b9c
Your Nameemail <pbplop@gmail.com>
2021-04-21 15:19:46 +0530

    made the home page single page

--- Client/src/components/Home.js
+++ Client/src/components/Home.js
@@ -4,17 +4,14 @@ import MenuX from './MenuX'
 import Item from './Item';
 import './Home.css';
 
-import One  from '../assets/1.jpg';
-import Two  from '../assets/2.jpg';
-import Plant  from '../assets/Plant.jpg';
 
 import Sonabags from '../assets/Sonabags.jpg';
-import Profile from '../assets/Profile.jpg';
+
 import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
 import './Home.css';
 
-import Sonabags2 from '../assets/Sonabags2.jpg';
+
 
 const Home = ({_notice, _RemoveNotice, text}) => {
 
@@ -39,91 +36,14 @@ const Home = ({_notice, _RemoveNotice, text}) => {
             desc=''
             descLink=''
             backgroundImg={Sonabags}
-            leftBtnText='Explore More'
-            leftBtnLink=''
-            rightBtnText='ModelX'
-            rightBtnLink=''
+            leftBtnText='Order Now'
+            leftBtnLink='/contact'
             
-            first
-            />
-
-            <Item 
-            title='Plant'
-            desc='Our plant is spread across 100 acres of land in
-
-            Bijauri, a strategic location at Dang that ensures easy availability of high-grade limestone.'
-            descLink=''
-            backgroundImg={Plant}
-            leftBtnText='Accessories'
-            leftBtnLink=''
-            rightBtnText='Accessories'
-            rightBtnLink=''
             
             
             />
 
-
-            <Item
-            title='ModelS'
-            desc='ModelS Description'
-            descLink=''
-            backgroundImg={Sonabags}
-            leftBtnText='ModelS'
-            leftBtnLink=''
-            rightBtnText='ModelS'
-            rightBtnLink=''
             
-            
-            />
-
-            <Item
-            title='Model3'
-            desc='Model3 Description'
-            descLink=''
-            backgroundImg={Sonabags2}
-            leftBtnText='Model3'
-            leftBtnLink=''
-            rightBtnText='Model3'
-            rightBtnLink=''
-            
-            
-            />
-
-            <Item
-            title='ModelY'
-            desc='ModelY Description'
-            descLink=''
-            backgroundImg={Two}
-            leftBtnText='ModelY'
-            leftBtnLink=''
-            rightBtnText='ModelY'
-            rightBtnLink=''
-            
-            
-            />
-
-            <Item
-            title='SolarPanels'
-            desc='SolarPanels Description'
-            descLink=''
-            backgroundImg={Plant}
-            leftBtnText='SolarPanels'
-            leftBtnLink=''
-            rightBtnText='SolarPanels'
-            rightBtnLink=''
-            
-            />
-
-            <Item
-            title='SolarRoof'
-            desc='SolarRoof Description'
-            descLink=''
-            backgroundImg={Plant}
-            leftBtnText='SolarRoof'
-            leftBtnLink=''
-            rightBtnText='SolarRoof'
-            rightBtnLink=''
-            />
         </div>
     </div>
     )
