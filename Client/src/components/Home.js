8b158d580d5ba0ab41856ac48165ceffda63ca5f
Your Nameemail <pbplop@gmail.com>
2021-04-21 20:14:03 +0530

    added a new homepage layout

--- Client/src/components/Home.js
+++ Client/src/components/Home.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react'
 import Header from './Header'
 import MenuX from './MenuX'
-import Item from './Item';
+import Profile from '../assets/profile.jpg'
 import './Home.css';
 
 
-import Sonabags from '../assets/Sonabags.jpg';
+import banner1 from '../assets/banner1.jpg';
 
 import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
@@ -17,6 +17,10 @@ const Home = ({_notice, _RemoveNotice, text}) => {
 
     return (
       <div className="App">
+        <link
+      href="https://unpkg.com/ionicons@4.5.10-0/dist/css/ionicons.min.css"
+      rel="stylesheet"
+    /> 
       <div>
         <Popup open={_notice} onClose={_RemoveNotice} modal>
 	  <div className="modal">
@@ -29,22 +33,49 @@ const Home = ({_notice, _RemoveNotice, text}) => {
       </div>
       <MenuX />
        <Header />
-      <div className="app_itemsContainer">
-            {/*To understand all these attributes inside of Item refer to the long ass comment in Item.js */}
-            <Item
-            title='WELCOME'
-            desc=''
-            descLink=''
-            backgroundImg={Sonabags}
-            leftBtnText='Order Now'
-            leftBtnLink='/contact'
+      
+       <div class="wrapper">
+      <div class="parallax__group hero-container">
+        <div class="parallax__layer sky"></div>
+        
+      
+        
+        <div class="parallax__layer people2"></div>
+        <div class="parallax__layer people3"></div>
+        <div class="parallax__layer hero-text">
+        
+          
+          <div class="year-container">
             
+          
             
-            
-            />
-
-            
+          </div>
+          <div class="social-container">
+            <a href="https://www.facebook.com/sonacement/" target="_blank"
+              ><i class="icon ion-logo-facebook"></i
+            ></a>
+            <a href="https://twitter.com/sonacement" target="_blank"
+              ><i class="icon ion-logo-twitter"></i
+            ></a>
+            <a href="/contact" target="_blank"
+              ><i class="icon ion-logo-google"></i
+            ></a>
+            <a href="https://www.instagram.com/sonacement/"target="_blank"
+              ><i class="icon ion-logo-instagram"></i
+            ></a>
+          </div>
         </div>
+      </div>
+      <div class="parallax__group info-container">
+        <img src={Profile} alt="Lively and colourful concert" />
+        <div class="text-container">
+          <h2>Sona Cement</h2>
+          <p>What would life be if we had no courage to attempt anything?</p>
+          <p>- Vincent Van Gogh</p>
+          <a href="#" class="btn">Order Now</a>
+        </div>
+      </div>
+    </div>
     </div>
     )
 }
