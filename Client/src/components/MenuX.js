8348ab44b409a90f48c78fdece04dd66d92d6efe
Anurag <vizculus@gmail.com>
2021-04-21 15:46:04 +0545

    Fixed burger menu problem

--- Client/src/components/MenuX.js
+++ Client/src/components/MenuX.js
@@ -12,7 +12,7 @@ import{
         return (
             <div>
                 {/* "right" pulls the menu from the right, put "left" if you want otherwise. "disableAutoFocus" prevents from automatic focus of the first menu item.*/}
-    <Menu right disableAutoFocus> 
+    <Menu burgerButtonClassName={"burger"} id = {"sidebar"} right disableAutoFocus> 
     {/* All the menu items. */}
     <Link to={'/'}>Home</Link>
     <Link to={'/about'}>About Us</Link>
